90185baae986274abc4fd39e5b432109f5a80c70
planT-444 <williamhu03@gmail.com>
2024-08-15 17:46:06 -0400

    added matrixToTable, backend sorting matrix by header column

--- index.js
+++ index.js
@@ -1,11 +1,61 @@
+table_grid = [
+    ['name', 'age', 'favfood', 'minecraft'],
+    ['beasdlfjdlj', '179', 'borgurh', 'veteran'],
+    ['wahahahah', '2', 'SPANAKOPITA', 'noob'],
+    ['AAAAAAAA', '69420', 'E', 'speedrunner']
+]
+
 $(document).ready(function() {
     const dataTypeSelect = $("#data-type")
     const dataTypeSubmit = $("#request-data-type")
     const dispatchButton = $("#send-dispatch-signal")
     const logContainer = $("#log-container")
     const log = $("#log")
+    const fetchedDataTable = $("#fetched-data-table")
 
     dispatchButton.attr("disabled", false) // for now
+
+    insertMatrixInTable(table_grid, fetchedDataTable)
+
+    console.log(table_grid)
+    console.log(sortMatrixByColumn(table_grid, 1))
+    
+
+    function insertMatrixInTable(matrix, table) {
+        for (let i = 0; i < matrix.length; i++) {
+            let row = matrix[i]
+            let newRowElement = $("<tr>")
+            for (let j = 0; j < matrix[0].length; j++) {
+                let cell = row[j]
+                let newCellElement = null;
+                if (i == 0) {
+                    newCellElement = $("<th>")
+                } else {
+                    newCellElement = $("<td>")
+                }
+                newCellElement.text(cell)
+                newRowElement.append(newCellElement)
+            }
+            table.append(newRowElement)
+        }
+    }
+
+    function sortMatrixByColumn(matrix, targetColumn) {
+        let tableHeaders = matrix.slice(0, 1)
+        let tableElements = matrix.slice(1)
+        let comparator = null
+        if ($.isNumeric(tableElements[0][targetColumn])) {
+            comparator = (rowA, rowB) => {
+                targetA = parseFloat(rowA[targetColumn])
+                targetB = parseFloat(rowB[targetColumn]) 
+                return targetA > targetB ? 1 : -1
+            }
+        } else {
+            comparator = (rowA, rowB) => rowA[targetColumn] > rowB[targetColumn] ? 1 : -1
+        }
+        tableElements.sort(comparator)
+        return tableHeaders.concat(tableElements)
+    }
 
     dataTypeSelect.change(dataTypeSelectChanged)
     dataTypeSubmit.click(userRequestDataType)
