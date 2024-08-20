53f7c6d1063bfa1716c9eea76f0ae60347441da6
planT-444 <williamhu03@gmail.com>
2024-08-20 16:09:24 -0400

    installed jquery

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require("express")
 const path = require("path")
 const port = 3000
 const restrictedFiles = ['/main.css', '/index.js']
-const domain = "http://localhost:3000"
+const domain = "localhost:3000"
 
 app = express()
 
