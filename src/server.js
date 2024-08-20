fc2df4881c48436ad7fe747c65ed2b6810364ea2
planT-444 <williamhu03@gmail.com>
2024-08-19 23:52:14 -0400

    restricted files now enforced!

--- src/server.js
+++ src/server.js
@@ -1,10 +1,26 @@
 const express = require("express")
 const path = require("path")
 const port = 3000
+const restrictedFiles = ['/main.css', '/index.js']
+const domain = "http://localhost:3000"
 
 app = express()
 
+app.use((req, res, next) => {
+    const referer = req.get("Referer")
+    console.log(req.path, referer)
+    if (restrictedFiles.includes(req.path) && !(referer && referer.includes(domain))) {
+        res.status(69420).send("RUH ROH RAGGY")
+    } else {
+        next()
+    }
+})
+
 app.use(express.static(path.join(__dirname, 'front-end')))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'front-end', 'index.html'))
+})
 
 app.listen(port, () => {
     console.log("listening on port ", port)
