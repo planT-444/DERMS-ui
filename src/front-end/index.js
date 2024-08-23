b69926290003ccd254aaf238896e0efa97970a12
planT-444 <williamhu03@gmail.com>
2024-08-23 18:28:32 -0400

    CHARTING WORKS

--- src/front-end/index.js
+++ src/front-end/index.js
@@ -5,17 +5,46 @@ $(function() {
     const logContainer = $("#log-container")
     const log = $("#log")
     const queryDataButton = $("#query-data-button")
+    const canvas = document.getElementById("load-data-chart").getContext('2d')
+
 
     // button event bindings
     dataTypeSelect.on("change", dataTypeSelectChanged)
     dataTypeSubmit.on("click", userRequestDataType)
     dispatchButton.on("click", userSendDispatchSignal)
     queryDataButton.on("click", async function() {await userQueryData()})
-    
+
+    const loadDataChart = new Chart(canvas, {
+        type: 'line',
+        data: {
+            labels: [],
+            datasets: [{
+                data: [],
+                label: 'load'
+            }]
+        },
+        options: {
+            scales: {
+                x: {
+                    title: {
+                        display: true,
+                        text: 'Time (s)'
+                    }
+                },
+                y: {
+                    beginAtZero: true,
+                    title: {
+                        display: true,
+                        text: 'Load (MW)'
+                    }
+                }
+            }
+        }
+    })
+
     // testing
     dispatchButton.attr("disabled", false) // for now
     
-
 
     function logMessage(msg) {
         let isScrolledToBottom = logContainer.scrollTop() > -5
@@ -57,10 +86,18 @@ $(function() {
             if (!response.ok)
                 throw new Error("ZERE VAS A PROHBLEM FETCHING ZE DATAH1!!1!")
             const data = await response.json()
-            console.log(data)
+            updateChart(data)
         } catch {
             console.log("ERRRRORRRR FETCHING ZE DATAH")
         }
+    }
+
+    function updateChart(data) {
+        console.log("YOOO DATA PASSED GIMMEH GIMME")
+        console.log(data)
+        loadDataChart.data.labels = data["time"]
+        loadDataChart.data.datasets[0].data = data["load"]
+        loadDataChart.update()
     }
 
     function DUMMYFUNKY() {
